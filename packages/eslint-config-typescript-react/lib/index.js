1a81ad41cbfb0f7a66fef9c2569d58222aa9e8e0
David Sichau <sichau@inf.ethz.ch>
2022-02-14 10:31:13 +0100

    ignore certain react rules in ts files

--- packages/eslint-config-typescript-react/lib/index.js
+++ packages/eslint-config-typescript-react/lib/index.js
@@ -5,9 +5,18 @@
 module.exports = {
   extends: ["@code-expert/react", "@code-expert/typescript"],
   rules: {
-    'react/prop-types': 'off',
-    'react/require-default-props': 'off',
+
 
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+        'react/require-default-props': 'off',
+        "react/jsx-key": "off",
+      },
+    },
+  ],
 
 };
