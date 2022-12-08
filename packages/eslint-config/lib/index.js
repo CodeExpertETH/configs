9350b97aef12d24e27ceb9ed2811b4f29cb40895
David Sichau <sichau@inf.ethz.ch>
2022-12-08 10:11:52 +0100

    added more rules

--- packages/eslint-config/lib/index.js
+++ packages/eslint-config/lib/index.js
@@ -13,6 +13,15 @@ module.exports = {
   ],
   rules: {
     'no-underscore-dangle': 'off',
+    'arrow-body-style': 'error',
+    'fp-ts/no-discarded-pure-expression': 'error',
+    'fp-ts/no-redundant-flow': 'error',
+    'fp-ts/prefer-bimap': 'error',
+    'fp-ts/prefer-chain': 'error',
+    'fp-ts/prefer-traverse': 'error',
+    'no-only-tests/no-only-tests': 'error',
+    'require-await': 'error',
+    'object-shorthand': 'error',
     'redos/no-vulnerable': 'error'
   }
 
